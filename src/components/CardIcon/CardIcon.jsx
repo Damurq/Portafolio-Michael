91969a1da42edb8a3e7378fb6a3f01d710467a58
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-03-11 00:24:11 -0400

    update styles

--- src/components/CardIcon/CardIcon.jsx
+++ src/components/CardIcon/CardIcon.jsx
@@ -9,7 +9,7 @@ const  CardIcon = () => {
             <a className="instagram social" href="https://www.instagram.com/mike.0408/" target="_blank" rel="noopener noreferrer">
                 <BsInstagram className="icon" />
             </a>
-            <a className="linkedin social" href="https://www.linkedin.com/in/michael-montero-urquiola-bb62b61b6" target="_blank" rel="noopener noreferrer">
+            <a className="linkedin social" href="https://www.linkedin.com/in/damurq/" target="_blank" rel="noopener noreferrer">
                 <FaLinkedinIn className="icon" />
             </a>
             <a className="github social" href="https://github.com/Damurq?tab=repositories" target="_blank" rel="noopener noreferrer">
